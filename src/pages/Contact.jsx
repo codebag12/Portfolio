43f637e5c9f8601f243006f2461b8a6fa9781867
codebag12 <sepreck@gmail.com>
2024-05-18 11:53:13 +0800

    more css changes

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
+import ContactComponent from "../components/Contact.jsx";
 
 export default function Contact() {
   return (
-    <div>Contact</div>
+    <ContactComponent />
   )
 }
