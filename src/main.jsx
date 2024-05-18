c3cc1fafb8bc4a940ca7f7c466729db4cdd63106
codebag12 <sepreck@gmail.com>
2024-05-18 10:46:42 +0800

    css changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,56 +1,47 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import Profile from './components/Profile'
-import Contact from './components/Contact.jsx/'
-import './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import Profile from "./components/Profile";
+import Contact from "./components/Contact.jsx/";
+import "./App.jsx";
 
-
-
-const experience =  {
-  'company_a' :{
-    name:'company aaa'
+const experience = {
+  company_a: {
+    name: "company aaa",
   },
 
-  'company_b' :{
-    name:'company b'
-  }
+  company_b: {
+    name: "company b",
+  },
+};
 
-   }
+const applications = ["google", "Globa", "Canva"];
 
-
-   const applications = ['google','Globa','Canva']
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <App />
 
+    <Profile
+      name="Tarzan Rambo"
+      position="bank robber"
+      experiences={experience}
+    />
 
-     <App /> 
-
-
-
-    <Profile name ="Tarzan Rambo" position="bank robber" experiences ={experience }/>
-
-   
-
-    {
-      (applications.length) && <>
-
-<h2>applications</h2>
-    <ul>
-      {
-        (applications.length) ? 
-      applications.map((item,index)=>{
-        return<li key={index}>{item}</li>
-      }
-    )
-    :<li>No Applications</li>
-      }
-    </ul>
-
+    {applications.length && (
+      <>
+        <h2>applications</h2>
+        <ul>
+          {applications.length ? (
+            applications.map((item, index) => {
+              return <li key={index}>{item}</li>;
+            })
+          ) : (
+            <li>No Applications</li>
+          )}
+        </ul>
       </>
-    }
+    )}
     <Contact />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
